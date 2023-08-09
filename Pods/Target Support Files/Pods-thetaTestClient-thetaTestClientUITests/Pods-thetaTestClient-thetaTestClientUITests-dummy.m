e82ac949f9da112a543729c0cb36cb2e0504e03c
yuehengshiCarmedis2p0 <yshi@carmedia2p0.com>
2023-08-09 11:24:25 -0400

    remove pod

--- Pods/Target Support Files/Pods-thetaTestClient-thetaTestClientUITests/Pods-thetaTestClient-thetaTestClientUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_thetaTestClient_thetaTestClientUITests : NSObject
-@end
-@implementation PodsDummy_Pods_thetaTestClient_thetaTestClientUITests
-@end
--- Pods/Target Support Files/Pods-thetaTestClientTests/Pods-thetaTestClientTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_thetaTestClientTests : NSObject
-@end
-@implementation PodsDummy_Pods_thetaTestClientTests
-@end
